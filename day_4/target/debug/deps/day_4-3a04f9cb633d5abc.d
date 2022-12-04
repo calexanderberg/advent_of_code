f529e2c08bf6dab890407c4c6ee7a0a2830c5544
calexanderberg <carlalexanderberg@gmail.com>
2022-12-04 15:09:24 +0100

    Cargo clean

--- day_4/target/debug/deps/day_4-3a04f9cb633d5abc.d
+++
@@ -1,5 +0,0 @@
-/Users/cab/Documents/Projects/adventOfCode22/day_4/target/debug/deps/day_4-3a04f9cb633d5abc.rmeta: src/main.rs
-
-/Users/cab/Documents/Projects/adventOfCode22/day_4/target/debug/deps/day_4-3a04f9cb633d5abc.d: src/main.rs
-
-src/main.rs:
--- day_4/target/debug/deps/day_4-ac6f5eb19af1d56a.d
+++
@@ -1,5 +0,0 @@
-/Users/cab/Documents/Projects/adventOfCode22/day_4/target/debug/deps/day_4-ac6f5eb19af1d56a.rmeta: src/main.rs
-
-/Users/cab/Documents/Projects/adventOfCode22/day_4/target/debug/deps/day_4-ac6f5eb19af1d56a.d: src/main.rs
-
-src/main.rs:
