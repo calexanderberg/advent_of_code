b6f39be5f8dbe6181dafea16e06ca2882e96254f
calexanderberg <carlalexanderberg@gmail.com>
2022-12-08 10:12:18 +0100

    Part 2 day 7

--- day_7/src/main.rs
+++ day_7/src/main.rs
@@ -1,7 +1,12 @@
+
 fn main() {
     let file = std::fs::read_to_string("input.txt").unwrap();
     let mut total = 0;
     let destroy_amount = 100000;
+
+    let total_space = 70000000;
+    let size_space = 30000000;
+    let mut dir_to_delete = vec![];
 
     let mut matrix = vec![("/", 0)];
     for line in file.lines().filter(|l| !l.is_empty()) {
@@ -12,29 +17,47 @@ fn main() {
         if line.starts_with("$ cd ") {
             let dir = &line[5..];
             if dir == ".." {
-                let (_, amount) = matrix.pop().unwrap();
+                let (name, amount) = matrix.pop().unwrap();
                 if amount <= destroy_amount {
                     total += amount;
                 } 
                 matrix.last_mut().unwrap().1 += amount;
+                dir_to_delete.push((name, amount));
             } else {
                 matrix.push((dir,0));
             }
-            continue;
+            continue; 
         }
 
-        let (amount, name) = line.split_once(" ").unwrap();
+        let (amount, _) = line.split_once(" ").unwrap();
 
         if let Ok(amount) = amount.parse::<usize>() {
             matrix.last_mut().unwrap().1 += amount;
         }
     }
 
+    while matrix.len() > 0 {
+        let (name, amount) = matrix.pop().unwrap();
+        dir_to_delete.push((name, amount));
+
+        if matrix.len() > 0 {
+            matrix.last_mut().unwrap().1 += amount;
+        }
+    }
+
+
+    let free_space = total_space - dir_to_delete.last().unwrap().1;
+    let required_space= size_space - free_space;
+
+    let part2 = dir_to_delete
+        .into_iter()
+        .filter(move |(_, amount)| *amount > required_space)
+        .map(|(_, amount)| {
+            return amount;
+        })
+        .min();
+
+
     println!("{:?}", total);
+    println!("{:?}", part2.unwrap());
 }
-
-
-/*
-I realized way too late this can be done recursively. Need to rewrite this. 
-*/
-
