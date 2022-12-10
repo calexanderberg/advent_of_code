e0b86f6bd42fd7493afa5fd521c36584247d37d6
calexanderberg <carlalexanderberg@gmail.com>
2022-12-10 14:59:51 +0100

    day 10 is awesome

--- day_10/target/debug/deps/day_10-6f22915a623c17eb.d
+++
@@ -1,5 +0,0 @@
-/Users/cab/Documents/Projects/adventOfCode22/day_10/target/debug/deps/day_10-6f22915a623c17eb.rmeta: src/main.rs
-
-/Users/cab/Documents/Projects/adventOfCode22/day_10/target/debug/deps/day_10-6f22915a623c17eb.d: src/main.rs
-
-src/main.rs:
--- day_10/target/debug/deps/day_10-c9638e030547463b.d
+++
@@ -1,5 +0,0 @@
-/Users/cab/Documents/Projects/adventOfCode22/day_10/target/debug/deps/day_10-c9638e030547463b.rmeta: src/main.rs
-
-/Users/cab/Documents/Projects/adventOfCode22/day_10/target/debug/deps/day_10-c9638e030547463b.d: src/main.rs
-
-src/main.rs:
